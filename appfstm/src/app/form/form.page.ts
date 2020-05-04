77775f2ccea6c72c69167837f65dbab89bcc9c40
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2020-05-04 18:05:05 +0800

    Ionic syntax function typescript

--- appfstm/src/app/form/form.page.ts
+++ appfstm/src/app/form/form.page.ts
@@ -11,5 +11,28 @@ export class FormPage implements OnInit {
 
   ngOnInit() {
   }
+  public num1:any;
+  public num2:any;
+  public output:any=0;
+  public n:any[] = new Array(3);
+  //function to perform add two numbers
+  performAdd(){
+    this.output=parseInt(this.num1)+parseInt(this.num2);
+	  this.displayresult();
 
+  }
+  performAddArray(){
+    for(var i = 0;i<this.n.length;i++) { 
+      this.output=this.output+parseInt(this.n[i]);
+      //console.log(this.n[i]); 
+    }
+	  this.displayresult();
+
+  }
+
+  displayresult(){
+    console.log("Output: "+this.output);
+    alert("Output: "+this.output);
+  }
+  
 }
