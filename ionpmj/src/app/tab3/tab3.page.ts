f81e2432a6a1f76d1e158aee02c5f5a35bb9d6ac
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-03-19 15:39:45 +0800

    ngModel binding calculate discount price

--- ionpmj/src/app/tab3/tab3.page.ts
+++ ionpmj/src/app/tab3/tab3.page.ts
@@ -9,4 +9,23 @@ export class Tab3Page {
 
   constructor() {}
 
+  //variable declaration
+  price:any;//bind to ion-input
+  productcode:any;
+  productname:any;
+  afterdisc:number;
+
+  calculatediscount(){
+    //discount 50%
+    this.afterdisc=parseFloat(this.price)-(parseFloat(this.price)*0.5);
+    alert ("Price after 50% discount = "+this.afterdisc);
+
+  }
+
+  //function helloword
+  helloworld(){
+    alert("Hello World!");
+    console.log("Selamat datang!!!");
+  }
+
 }
